d4e3c3859ec3c6b661dbfe64f7aa978450230971
Jorge <jorge.pinto.plaza@gmail.com>
2021-03-05 12:25:26 +0100

    Segundo Commit

---
+++ LibreraMVC1/src/es/laarboleda/tiendaLibros/Nueva.java
@@ -0,0 +1,10 @@
+package es.laarboleda.tiendaLibros;
+
+public class Nueva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Buenos dias");
+	}
+
+}
